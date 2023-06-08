55108de2567b8ad0ee07c89d60fe4931df776dff
Aman Ghanghoriya <121393968+amonsoon@users.noreply.github.com>
2023-06-08 11:51:10 +0530

    add feature - update and delete confirmation  modals

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -4,14 +4,20 @@ import { useSelector, useDispatch } from "react-redux";
 import { addTodo, deleteTodo, setTodos, updateTodo } from "../features/todo/todoSlice";
 import { Edit2, Trash } from "react-feather";
 import axios from "axios";
+import Modal from 'react-modal';
 
 import emptyImage from '../images/empty.svg'
+import { formBtnStyle } from "../utils/groupClasses";
 
 type Props = {}
 
 function Todo({ }: Props) {
   const [text, setText] = useState('');
   const [updateIndex, setUpdateIndex] = useState<number | null>(null)
+  const [deleteIndex, setDeleteIndex] = useState<number | null>(null)
+
+  const [isDeleteModalOpen, setDeleteModalOpen] = useState(false);
+  const [isUpdateModalOpen, setUpdateModalOpen] = useState(false);
 
   const dispatch = useDispatch();
   const { todos } = useSelector((state: RootState) => state.todo)
@@ -36,28 +42,68 @@ function Todo({ }: Props) {
       }
       dispatch(addTodo({ todo }));
       createTodoDB(todo);
+      setText('')
     }
     else {
-      const todo = { ...todos[updateIndex], text }
-      dispatch(updateTodo({ updateIndex, todo }))
-      updateTodoDB(todo.todoId, todo.text)
+      setUpdateModalOpen(true);
     }
-
-    setText('')
-    setUpdateIndex(null)
   }
 
+  // Deleting todo item.
   const handleDelete = (index: number) => {
-    dispatch(deleteTodo({ todoIndex: index }))
-    setUpdateIndex(null)
-    setText('')
-    deleteNoteDB(todos[index].todoId as string)
+    setDeleteModalOpen(true);
+    setDeleteIndex(index)
   }
 
+  const onDeleteConfirm = () => {
+    console.log("confirm delete")
+    if (deleteIndex !== null) {
+      dispatch(deleteTodo({ todoIndex: deleteIndex }))
+      deleteNoteDB(todos[deleteIndex].todoId)
+
+      setUpdateIndex(null)
+      setDeleteIndex(null)
+      setText('')
+    }
+    setDeleteModalOpen(false)
+  }
+
+  const onDeleteCancel = () => {
+    setUpdateIndex(null)
+    setDeleteIndex(null)
+    setText('')
+
+    setDeleteModalOpen(false)
+  }
+
+  // Updating todo item.
   const handleEdit = (index: number) => {
     setUpdateIndex(index)
     setText(todos[index].text)
   }
+
+  const onUpdateConfirm = () => {
+    if (updateIndex !== null && text) {
+      const todo = { ...todos[updateIndex], text }
+      dispatch(updateTodo({ updateIndex, todo }))
+      updateTodoDB(todo.todoId, todo.text);
+
+      setUpdateIndex(null)
+      setText('')
+    }
+    setUpdateModalOpen(false)
+  }
+
+  const onUpdateCancel = () => {
+    setUpdateIndex(null)
+    setText('')
+    setUpdateModalOpen(false)
+  }
+
+
+
+
+
 
   // fetch all todos
   const fetchAllTodoDB = async () => {
@@ -136,37 +182,83 @@ function Todo({ }: Props) {
   }
 
 
+  // modal custom style
+  const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      padding: 0,
+      border: 'none',
+      background: 'transparent'
+    },
+  };
+
+
   return (
-    <div className="flex flex-col mx-auto mt-0 sm:mt-4 max-w-[600px] w-full">
-      <div className="p-4">
-        <div className="bg-white p-2 border-2 mb-5 rounded-full transition-all focus-within:shadow-lg focus-within:shadow-slate-100">
-          <form className="flex" onSubmit={handleSubmit}>
-            <input className="min-w-[150px] grow py-3 px-4 bg-transparent rounded-md outline-none transition-all" type="text" placeholder="Write something..." value={text} onChange={(e) => setText(e.target.value)} />
-            <button className="px-4 py-2.5 bg-black text-white leading-none rounded-full"> {updateIndex === null ? 'Submit' : 'Update'} </button>
-          </form>
-        </div>
-        <div className="">
-          {todos.length > 0 ? (
-            <ul className="flex flex-col gap-3">
-              {todos.map(({ text }, index) => (
-                <li className={`group relative flex items-start py-3.5 px-4 bg-slate-100 rounded-lg overflow-hidden outline ${updateIndex === index ? "outline-3 outline-green-200 bg-green-50" : "outline-1 outline-slate-300"}`} key={index}>
-                  <div> {text} </div>
-                  <div className="absolute top-0 right-0 p-1 rounded-b-lg bg-white/80 border border-white flex items-center opacity-0 group-hover:opacity-100">
-                    <button className="p-2 rounded-lg text-slate-500 border border-transparent hover:bg-orange-50 hover:text-orange-600 hover:border-orange-200" onClick={() => handleEdit(index)} > <Edit2 size={16} strokeWidth={1.5} /> </button>
-                    <button className="p-2 rounded-lg text-slate-500 border border-transparent hover:bg-red-50 hover:text-red-600 hover:border-red-200" onClick={() => handleDelete(index)}  > <Trash size={16} strokeWidth={1.5} /> </button>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <div className="py-4 flex flex-col items-center">
-              <div className="text-2xl sm:text-3xl text-gray-300 font-bold mb-6"> Todo list is Empty.  </div>
-              <img src={emptyImage} alt="dodo list is empty" className="w-[150px] sm:w-[200px]" />
-            </div>
-          )}
+    <>
+      <div className="flex flex-col mx-auto mt-0 sm:mt-4 max-w-[600px] w-full">
+        <div className="p-4">
+          <div className="bg-white p-2 border-2 mb-5 rounded-full transition-all focus-within:shadow-lg focus-within:shadow-slate-100">
+            <form className="flex" onSubmit={handleSubmit}>
+              <input className="min-w-[150px] grow py-3 px-4 bg-transparent rounded-md outline-none transition-all" type="text" placeholder="Write something..." value={text} onChange={(e) => setText(e.target.value)} />
+              <button className="px-4 py-2.5 bg-black text-white leading-none rounded-full"> {updateIndex === null ? 'Submit' : 'Update'} </button>
+            </form>
+          </div>
+          <div className="">
+            {todos.length > 0 ? (
+              <ul className="flex flex-col gap-3">
+                {todos.map(({ text }, index) => (
+                  <li className={`group relative flex items-start py-3.5 px-4 bg-slate-100 rounded-lg overflow-hidden outline ${updateIndex === index ? "outline-3 outline-green-200 bg-green-50" : "outline-1 outline-slate-300"}`} key={index}>
+                    <div> {text} </div>
+                    <div className="absolute top-0 right-0 p-1 rounded-b-lg bg-white/80 border border-white flex items-center opacity-0 group-hover:opacity-100">
+                      <button className="p-2 rounded-lg text-slate-500 border border-transparent hover:bg-orange-50 hover:text-orange-600 hover:border-orange-200" onClick={() => handleEdit(index)} > <Edit2 size={16} strokeWidth={1.5} /> </button>
+                      <button className="p-2 rounded-lg text-slate-500 border border-transparent hover:bg-red-50 hover:text-red-600 hover:border-red-200" onClick={() => handleDelete(index)}  > <Trash size={16} strokeWidth={1.5} /> </button>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <div className="py-4 flex flex-col items-center">
+                <div className="text-2xl sm:text-3xl text-gray-300 font-bold mb-6"> Todo list is Empty.  </div>
+                <img src={emptyImage} alt="dodo list is empty" className="w-[150px] sm:w-[200px]" />
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+      {/* delete confirmation modal */}
+      <Modal
+        isOpen={isDeleteModalOpen}
+        style={customStyles}
+        contentLabel="Todo delete confirmation modal"
+      >
+        <div className="p-4 sm:p-6 border-2 rounded-lg bg-white">
+          <div className="mb-4"> Are you sure you want to delete ? </div>
+          <div className="flex justify-end gap-2">
+            <button className='px-4 py-3 text-sm leading-none bg-black text-white rounded-lg' onClick={onDeleteCancel}> No </button>
+            <button className='px-4 py-3 text-sm leading-none bg-black text-white rounded-lg' onClick={onDeleteConfirm}> Yes </button>
+          </div>
+        </div>
+      </Modal>
+      {/* update confirmation modal */}
+      <Modal
+        isOpen={isUpdateModalOpen}
+        style={customStyles}
+        contentLabel="Todo update confirmation modal"
+      >
+        <div className="p-4 sm:p-6 border-2 rounded-lg bg-white">
+          <div className="mb-4"> Are you sure you want to update ? </div>
+          <div className="flex justify-end gap-2">
+            <button className='px-4 py-3 text-sm leading-none bg-black text-white rounded-lg' onClick={onUpdateCancel}> No </button>
+            <button className='px-4 py-3 text-sm leading-none bg-black text-white rounded-lg' onClick={onUpdateConfirm}> Yes </button>
+          </div>
+        </div>
+      </Modal>
+    </>
   )
 }
 
